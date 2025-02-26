53689dafb8072b1f3b78fc103ece6f3567191e7d
NeilJain1 <neil444455555@gmail.com>
2025-02-26 16:04:00 +0000

    a

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,1 +1,30 @@
 package org.example; //DO NOT DELETE
+/*
+GallonConverter
+Neil Jain
+*/
+import java.util.Scanner ;
+public class Main {
+    public static void main(String[] args) {
+        double gallons;
+        double quarts;
+        double pints;
+        double cups;
+        double tablespoons;
+        Scanner input = new Scanner(System.in) ;
+        System.out.print("Enter the number of gallons: ") ;
+        gallons = input.nextDouble() ;
+        input.close() ;
+        System.out.println("In 4.0 gallons there are:") ;
+        quarts = gallons * 4.0 ;
+        pints = gallons * 8.0 ;
+        cups = pints * 2.0 ;
+        tablespoons = cups * 16.0 ;
+        System.out.println(quarts + " quarts") ;
+        System.out.println(pints + " pints") ;
+        System.out.println(cups + " cups") ;
+        System.out.println(tablespoons + " tablespoons") ;
+
+        
+    }
+}
